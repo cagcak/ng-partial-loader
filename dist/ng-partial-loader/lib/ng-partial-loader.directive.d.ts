ff820585d7e42568b020125b32dfb7c00659d3f2
cagcak <cagcak@outlook.com>
2021-12-19 18:18:02 +0300

    1.1.1

--- dist/ng-partial-loader/lib/ng-partial-loader.directive.d.ts
+++ dist/ng-partial-loader/lib/ng-partial-loader.directive.d.ts
@@ -1,18 +1,18 @@
 import { ElementRef, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
 import * as i0 from "@angular/core";
-interface Options {
+export declare type NgPartialLoaderOptions = Partial<{
     path: string;
-    fallback?: string;
-    customLoader?: string;
-    minHeight?: string;
-    loader?: 'blocks' | 'dual-ring' | 'pulse' | 'rolling' | 'spin' | 'cube' | 'double-ring' | 'ellipsis' | 'ripple' | 'spinner';
-}
+    fallback: string;
+    customLoader: string;
+    minHeight: string;
+    loader: 'blocks' | 'dual-ring' | 'pulse' | 'rolling' | 'spin' | 'cube' | 'double-ring' | 'ellipsis' | 'ripple' | 'spinner';
+}>;
 export declare class NgPartialLoaderDirective implements OnInit, OnChanges {
     private targetEl;
     private renderer;
     hostPosition: string;
     ngPartialLoader: boolean;
-    loaderOptions: Options;
+    loaderOptions: NgPartialLoaderOptions;
     uuid: string;
     private get utils();
     constructor(targetEl: ElementRef, renderer: Renderer2);
@@ -21,4 +21,3 @@ export declare class NgPartialLoaderDirective implements OnInit, OnChanges {
     static ɵfac: i0.ɵɵFactoryDeclaration<NgPartialLoaderDirective, never>;
     static ɵdir: i0.ɵɵDirectiveDeclaration<NgPartialLoaderDirective, "[ngPartialLoader]", never, { "ngPartialLoader": "ngPartialLoader"; "loaderOptions": "loaderOptions"; }, {}, never>;
 }
-export {};
